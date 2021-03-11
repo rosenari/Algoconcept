87157f1d467c914b2b9db277d974d2dd5a99fc3a
임장순 <rosenari88@gmail.com>
2021-03-11 12:48:33 +0900

    Add / 버블정렬

---
+++ 버블정렬/index.js
@@ -0,0 +1,16 @@
+(function () {
+    let arr = [7, 5, 3, 1, 2];
+    const N = arr.length;
+    for (let i = N - 1; i > 0; i--) {
+
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    console.log(arr);
+})();
